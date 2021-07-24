b95c7c425eef36056526e4a59b8ab7b023dfa497
DmitriySerobyan <DmitriySerobyan@yandex.ru>
2021-07-24 16:10:49 +0500

    parser test refactoring

--- src/test/java/ru/serobyan/json/lexer/ParserTest.java
+++ src/test/java/ru/serobyan/json/lexer/ParserTest.java
@@ -4,45 +4,45 @@ import lombok.SneakyThrows;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.serobyan.json.parser.Parser;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 class ParserTest {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("cases")
     @SneakyThrows
-    public void parseObject() {
-        var json = "{\"v1\":1,\"v2\":\"test\"\"v3\":[1]}";
-        var expected = Map.of(
-            "v1", 1,
-            "v2", "test",
-            "v3", List.of(1)
+    public void parse(String json, Object expectedParsed) {
+        var parser = new Parser(json);
+        var result = parser.parse();
+        MatcherAssert.assertThat(result, Matchers.equalTo(expectedParsed));
+    }
+
+    private static Stream<Arguments> cases() {
+        return Stream.of(
+            Arguments.of(
+                "{\"v1\":1,\"v2\":\"test\"\"v3\":[1]}",
+                Map.of(
+                    "v1", 1,
+                    "v2", "test",
+                    "v3", List.of(1)
+                )
+            ),
+            Arguments.of(
+                "[1,2]",
+                List.of(1, 2)
+            ),
+            Arguments.of(
+                "1",
+                1
+            )
         );
-        var parser = new Parser(json);
-        var result = parser.parse();
-        MatcherAssert.assertThat(result, Matchers.equalTo(expected));
-    }
-
-    @Test
-    @SneakyThrows
-    public void parseArray() {
-        var json = "[1,2]";
-        var expected = List.of(1, 2);
-        var parser = new Parser(json);
-        var result = parser.parse();
-        MatcherAssert.assertThat(result, Matchers.equalTo(expected));
-    }
-
-    @Test
-    @SneakyThrows
-    public void parsePrimitive() {
-        var json = "1";
-        var expected = 1;
-        var parser = new Parser(json);
-        var result = parser.parse();
-        MatcherAssert.assertThat(result, Matchers.equalTo(expected));
     }
 
 }
